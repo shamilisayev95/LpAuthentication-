415a10f9153258d24bbf8c66ef6a32dbdcf5db08
VictoriaMarjina <61832815+VictoriaMarjina@users.noreply.github.com>
2020-12-01 08:38:44 +0400

    Viktoriya

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
 import './style.less';
+
+class View {
+    constructor() {
+        this._userLogin = null;
+        this._userPassword = null;
+        this._signInButton = null;
+        this._signUpButton = null;
+        this.init();
+    }
+    init = () => {
+        this._signUpButton = document.createElement("div");
+        this._userLogin = document.createElement("div");
+        this._userPassword = document.createElement("div");
+        this._signInButton = document.createElement("div");
+        this._signInButton.addEventListener('click', this.);
+        this._signUpButton.addEventListener('click', this.);
+    };
+};
